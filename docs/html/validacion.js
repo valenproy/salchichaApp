6e11a395a4e84991c754f18bc146e71cfa2bb0f1
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-03-11 20:17:44 -0300

    validacion version 2

--- docs/html/validacion.js
+++ docs/html/validacion.js
@@ -1,6 +1,19 @@
-function validacion ()  {
 
+
+function validacion ()  {
+if(document.form.email.value===''){
+    document.form.email.focus()
+    console.log('el campo email esta vasio')
+    return false
+}
+
+
+if(document.form.nombre.value===''){
+    document.form.nombre.focus()
+console.log('el campo nombre esta vasio')
+}
 
 console.log('soy la funcion validacion')
+return false
 
 }
