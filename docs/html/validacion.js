cb174226fe0ecb963f06d2a0dee0a85cb1c6fd7a
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-03-18 19:32:13 -0300

    terminando la validasion

--- docs/html/validacion.js
+++ docs/html/validacion.js
@@ -1,19 +1,34 @@
 
+var eleForm = document.getElementById('mensajeForm')
+eleForm.style.display='none'
 
 function validacion ()  {
-if(document.form.email.value===''){
+eleForm.style.display='none'
+
+    if(document.form.email.value===''){
     document.form.email.focus()
-    console.log('el campo email esta vasio')
+    eleForm.style.display='inline'
+    eleForm.innerHTML='el campo email es requerido'
+    
     return false
 }
 
 
 if(document.form.nombre.value===''){
     document.form.nombre.focus()
-console.log('el campo nombre esta vasio')
+    eleForm.style.display='inline'
+    eleForm.innerHTML='el campo nombre es requerido'
+
+    return false
+}
+if(document.form.comentarios.value===''){
+document.form.comentarios.focus()
+eleForm.style.display='inline'
+eleForm.innerHTML='el campo comentario es requerido'
+return false
 }
 
-console.log('soy la funcion validacion')
+
 return false
 
 }
