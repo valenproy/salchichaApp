be47605abca3377b563b214b3af619c61e7d24a8
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2021-03-25 19:45:54 -0300

    mostrando los valores

--- docs/html/validacion.js
+++ docs/html/validacion.js
@@ -2,6 +2,23 @@
 var eleForm = document.getElementById('mensajeForm')
 eleForm.style.display='none'
 
+function callphp(){
+    
+    let data = {nombre:'valen'}
+
+    fetch('https://example.com/profile', { method: 'POST', 
+     headers: { 'Content-Type': 'application/json', }, 
+     body: JSON.stringify(data), }) 
+     .then(response => response.json()) 
+     .then(data => { console.log('Success:', data); }) 
+     .catch((error) => { console.error('Error:', error); });
+
+     return false
+
+}
+
+
+
 function validacion ()  {
 eleForm.style.display='none'
 
@@ -9,26 +26,35 @@ eleForm.style.display='none'
     document.form.email.focus()
     eleForm.style.display='inline'
     eleForm.innerHTML='el campo email es requerido'
-    
     return false
-}
+    } else {
+    console.log(document.form.email.value)
+    }
 
 
 if(document.form.nombre.value===''){
     document.form.nombre.focus()
     eleForm.style.display='inline'
     eleForm.innerHTML='el campo nombre es requerido'
-
     return false
+} else {
+    console.log(document.form.nombre.value)
+
 }
+
+
+
 if(document.form.comentarios.value===''){
 document.form.comentarios.focus()
 eleForm.style.display='inline'
 eleForm.innerHTML='el campo comentario es requerido'
 return false
+} else {
+    console.log(document.form.comentarios.value)
+
 }
 
 
-return false
+return callphp()
 
 }
